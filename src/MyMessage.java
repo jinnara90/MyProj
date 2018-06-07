c09a72356af3e7607d040cbf44f479977e6b493c
unknown <dnflsrjdnf1@gmail.com>
2018-06-07 09:29:51 +0900

    Hello.java

--- src/MyMessage.java
+++ src/MyMessage.java
@@ -1,9 +1,13 @@
 public class MyMessage {
 	String message;
 	public MyMessage(String message) {
+
 		this.message = message;
+
 	}
 	public void print() {
+
 		System.out.println(message);
+
 	}
 }
